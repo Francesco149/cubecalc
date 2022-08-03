7ce49c6fde32685272a6c26a799556c14a4ff35a
Franc[e]sco <lolisamurai@tfwno.gf>
2022-08-03 11:46:55 +0200

    add cape, belt, shoulder with violets

--- cubecalc.py
+++ cubecalc.py
@@ -221,6 +221,18 @@ lines_accessory_violet = [
   (N, ALLSTAT, 6, 1/7.84*100),
 ]
 
+prime_lines_cape_belt_shoulder_violet = [
+  (P, STAT, 12, 1/8.89*100),
+  (P, HP, 12, 1/8.89*100),
+  (P, ALLSTAT, 9, 1/6.67*100),
+]
+
+lines_cape_belt_shoulder_violet = [
+  (N, STAT, 9, 1/8.47*100),
+  (N, HP, 9, 1/10.17*100),
+  (N, ALLSTAT, 6, 1/6.78*100),
+]
+
 def filter_impossible_lines(combos):
   for combo in combos:
     counts = {BOSS: 0, IED: 0}
@@ -409,3 +421,4 @@ cube_calc_o("accessory (occult cubes)",
   prime_lines_occult_accessory, lines_occult_accessory, combos_occult_stat)
 
 cube_calc_v("accessory (violets)", prime_lines_accessory_violet, lines_accessory_violet, combos_stat)
+cube_calc_v("cape, belt, shoulder (violets)", prime_lines_cape_belt_shoulder_violet, lines_cape_belt_shoulder_violet, combos_stat)
